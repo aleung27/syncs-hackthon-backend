e024e5dd272f179a4fb9975d6a7362eb9c1f9bbd
Sashi Peiris <cpei5953@uni.sydney.edu.au>
2020-08-30 11:41:43 +1200

    Bug fix for audio

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 var app = require("express")();
-var express = require("express");
+var express = require('express');
 
 //var http = require("http").createServer(app);
-var https = require("https");
+var https = require('https');
 
-var fs = require("fs");
-var socket = require("socket.io");
-var privateKey = fs.readFileSync("key.pem", "utf-8");
-var certificate = fs.readFileSync("cert.pem", "utf-8");
-var passphrase = "1234";
-var credentials = {
-  key: privateKey,
-  cert: certificate,
-  passphrase: passphrase,
-};
+var fs = require('fs');
+var socket = require('socket.io')
+var privateKey = fs.readFileSync('key.pem', 'utf-8');
+var certificate = fs.readFileSync('cert.pem', 'utf-8');
+var passphrase = '1234';
+var credentials = { key: privateKey, cert: certificate, passphrase: passphrase };
 
 //var io = require("socket.io")(http);
 var httpsServer = https.createServer(credentials, app);
 
 var io = socket(httpsServer);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 const cors = require("cors");
@@ -39,16 +35,14 @@ var users = [];
 //    "username": username
 //    "x": x,
 //    "y": y,
-//    "volumes" : {}
+//    "volumes": {},
 // }
+
 
 var images = [];
 // Store custom user sprites
-var bg = "atlas";
-var bgURLs = {
-  atlas:
-    "https://cdn.discordapp.com/attachments/747990497091518557/749222951865286676/networking_event-01-01.png",
-};
+var bg = "atlas"
+var bgURLs = {"atlas" : "https://cdn.discordapp.com/attachments/747990497091518557/749222951865286676/networking_event-01-01.png"}
 
 const imgURLs = [
   "https://cdn.discordapp.com/attachments/747990497091518557/749221522740346890/eva-01-01.png",
@@ -59,7 +53,6 @@ const imgURLs = [
 
 // Stores all the sockets
 var sockets = [];
-
 var comments = [
   {
     username: "Adam",
@@ -91,104 +84,17 @@ const emitAll = () => {
 
 // Emits new position to all sockets
 const emitAllImage = () => {
-  sockets.forEach((s) => {
-    console.log("Emit All Image");
-    s.emit("image", {
-      userData: users,
-      bgURL: bgURLs[bg],
-      width: WIDTH,
-      height: HEIGHT,
-      imageData: images,
+    sockets.forEach((s) => {
+      console.log("Emit All Image")
+      s.emit("image", {userData: users, bgURL: bgURLs[bg], width: WIDTH, height: HEIGHT, imageData: images});
     });
-  });
-};
+  };
 
-const emitAllComments = () => {
-  sockets.forEach((s) => {
-    s.emit("getComment", comments);
-  });
-};
-
-// io.on("connection", (socket) => {
-//   console.log("connected");
-
-//   console.log("new");
-//   io.sockets.emit(
-//     "user-joined",
-//     socket.id,
-//     io.engine.clientsCount,
-//     Object.keys(io.sockets.clients().sockets)
-//   );
-
-//   socket.on("signal", (toId, message) => {
-//     io.to(toId).emit("signal", socket.id, message);
-//   });
-
-//   socket.on("message", function (data) {
-//     io.sockets.emit("broadcast-message", socket.id, data);
-//   });
-
-//   socket.on("disconnect", function () {
-//     io.sockets.emit("user-left", socket.id);
-//   });
-
-//   var userID = users.length;
-//   // Add the socket and the users to the list
-//   sockets.push(socket);
-//   users.push({
-//     id: userID,
-//     sid: asocketId,
-//     username: null,
-//     x: Math.random() * (WIDTH - 200) + 100,
-//     y: Math.random() * (HEIGHT - 200) + 100,
-//     userImg: imgURLs[userID % 4],
-//     volumes: {} // id: volume pairs
-//     //userImg: imgURLs[Math.random() * imgURLs.length],
-//   });
-
-//   emitAllImage();
-//   // Tell the connected user their id & emit all positions
-//   socket.emit("id", userID);
-//   socket.emit("position", users);
-//   socket.emit("getComment", comments);
-
-//   // Movement commands
-//   socket.on("move", (data) => {
-//     console.log("Move Received");
-//     switch (data.direction) {
-//       case "left":
-//         users[data.id].x -= 5;
-//         emitAll();
-//         break;
-//       case "right":
-//         users[data.id].x += 5;
-//         emitAll();
-//         break;
-//       case "up":
-//         users[data.id].y -= 5;
-//         emitAll();
-//         break;
-//       case "down":
-//         users[data.id].y += 5;
-//         emitAll();
-//         break;
-//     }
-//   });
-
-//   socket.on("setName", (data) => {
-//     users[data.id].username = data.username;
-//   });
-
-//   socket.on("comment", (data) => {
-//     comments.push(data);
-//     emitAllComments();
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("Disconnected");
-//     // TODO: remove user and socket
-//   });
-// });
+  const emitAllComments = () => {
+    sockets.forEach((s) => {
+      s.emit("getComment", comments);
+    });
+  };
 
 io.on("connection", (socket) => { // New connection start
     console.log("connected");
@@ -240,6 +146,7 @@ io.on("connection", (socket) => { // New connection start
         // Tell the connected user their id & emit all positions
         socket.emit("id", userID);
         socket.emit("position", users);
+        socket.emit("getComment", comments);
     
           // Movement commands
           socket.on("move", (data) => {
@@ -294,6 +201,7 @@ io.on("connection", (socket) => { // New connection start
 });
 
 
+
 app.get("/rooms", cors(), (req, res) => {
   return res.send(rooms);
 });
@@ -302,25 +210,26 @@ httpsServer.listen(PORT, () => {
   console.log("listening...");
 });
 
+
 var volAdjust = function(id, nUsers) {
-    var vols = {};
-    console.log("Adjusting Volume")
-    for (var id1 = 0; id1 < nUsers; id1++) {
-      for (var id2 = 0; id2 < nUsers; id2++) {
-          if (id1 != id2) {
-            var newVol = volumeControl(distance(id1, id2));
-            users[id1].volumes[users[id2].sid] = newVol;
-          }
-      }
-      
+  var vols = {};
+  console.log("Adjusting Volume")
+  for (var id1 = 0; id1 < nUsers; id1++) {
+    for (var id2 = 0; id2 < nUsers; id2++) {
+        if (id1 != id2) {
+          var newVol = volumeControl(distance(id1, id2));
+          users[id1].volumes[users[id2].sid] = newVol;
+        }
     }
-    // for (var i = 0; i < nUsers; i++) {
-    //   if (i != id) {
-    //     var currVol = volumeControl(distance(id, i));
-    //     users[i].volumes[users[id].sid] = currVol;
-    //     vols[users[i].sid] = currVol;
-  
-    //   }
-    //}
-    //users[id].volumes = vols;
+    
+  }
+  // for (var i = 0; i < nUsers; i++) {
+  //   if (i != id) {
+  //     var currVol = volumeControl(distance(id, i));
+  //     users[i].volumes[users[id].sid] = currVol;
+  //     vols[users[i].sid] = currVol;
+
+  //   }
+  //}
+  //users[id].volumes = vols;
 }
