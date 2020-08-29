fec310642eae8c42b1a842cfbfe15efb448dacf9
adam <adamjleung123@gmail.com>
2020-08-29 22:23:11 +1000

    Profile pics

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 var app = require("express")();
-var express = require('express');
+var express = require("express");
 
 var http = require("http").createServer(app);
-var https = require('https');
-var fs = require('fs');
+var https = require("https");
+var fs = require("fs");
 var io = require("socket.io")(http);
-var socket = require('socket.io')
+var socket = require("socket.io");
 
 const cors = require("cors");
 
-var privateKey = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('cert.pem', 'utf8');
-var passphrase = '1234';
-var credentials = { key: privateKey, cert: certificate, passphrase: passphrase };
+var privateKey = fs.readFileSync("key.pem", "utf8");
+var certificate = fs.readFileSync("cert.pem", "utf8");
+var passphrase = "1234";
+var credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: passphrase,
+};
 
 //var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 var io = socket(httpsServer);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 const PORT = 3030;
 const WIDTH = 1200;
 const HEIGHT = 800;
-const evaPFP =
-  "https://cdn.discordapp.com/attachments/747990497091518557/749221522740346890/eva-01-01.png";
 
 var users = [];
 // Users is of the form
@@ -39,13 +41,11 @@ var users = [];
 //    "y": y,
 // }
 
-var imgURLs = [
-  evaPFP,
-  evaPFP,
-  evaPFP,
-  "https://www.w3schools.com/images/lamp.jpg",
-  "https://www.w3schools.com/images/lamp.jpg",
-  "https://www.w3schools.com/images/lamp.jpg",
+const imgURLs = [
+  "https://cdn.discordapp.com/attachments/747990497091518557/749221522740346890/eva-01-01.png",
+  "https://cdn.discordapp.com/attachments/747990497091518557/749240320159383642/SEAN.png",
+  "https://cdn.discordapp.com/attachments/747990497091518557/749238070854942800/adam-01.png",
+  "https://cdn.discordapp.com/attachments/747990497091518557/749237398046507037/judd-01.png",
 ]; // Please add image URLs
 
 // Stores all the sockets
@@ -74,20 +74,25 @@ httpsServer.listen(PORT, () => {
 io.on("connection", (socket) => {
   console.log("connected");
 
-  console.log("new")
-	io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+  console.log("new");
+  io.sockets.emit(
+    "user-joined",
+    socket.id,
+    io.engine.clientsCount,
+    Object.keys(io.sockets.clients().sockets)
+  );
 
-	socket.on('signal', (toId, message) => {
-		io.to(toId).emit('signal', socket.id, message);
-  	});
+  socket.on("signal", (toId, message) => {
+    io.to(toId).emit("signal", socket.id, message);
+  });
 
-    socket.on("message", function(data){
-		io.sockets.emit("broadcast-message", socket.id, data);
-    })
+  socket.on("message", function (data) {
+    io.sockets.emit("broadcast-message", socket.id, data);
+  });
 
-	socket.on('disconnect', function() {
-		io.sockets.emit("user-left", socket.id);
-	})
+  socket.on("disconnect", function () {
+    io.sockets.emit("user-left", socket.id);
+  });
 
   var userID = users.length;
   // Add the socket and the users to the list
@@ -97,7 +102,7 @@ io.on("connection", (socket) => {
     username: null,
     x: Math.random() * (WIDTH - 200) + 100,
     y: Math.random() * (HEIGHT - 200) + 100,
-    userImg: imgURLs[userID],
+    userImg: imgURLs[Math.random() * imgURLs.length],
   });
 
   emitAllImage();
